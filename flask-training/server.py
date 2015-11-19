76f3d318e1f2819a01e8859beee4283a57419d8d
David Hilburger <letitbespliced7@gmail.com>
2015-11-19 10:06:05 -0500

    adding url variable pratice

--- flask-training/server.py
+++ flask-training/server.py
@@ -10,6 +10,18 @@ def index():
 def hello_world():
 	return "<b>hello world</b>"
 
+@app.route('/user/<username>')
+def user(username):
+	return "hello ,%s" % username
+
+@app.route('/entityid/<int:id>')
+def entity(id):
+	return "Post: %d" % id
+
+@app.route('/entityid/<invalid>')
+def entity_invalid(invalid):	
+	return "Please use a proper integer for the id"
+
 if __name__ == "__main__": 
 	app.run(host='0.0.0.0')
 
