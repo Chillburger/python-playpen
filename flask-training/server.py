b3f04ffb91605b92669cdb7692d70f8ed3912041
David Hilburger <letitbespliced7@gmail.com>
2015-11-19 09:44:44 -0500

    adding multiple urls

--- flask-training/server.py
+++ flask-training/server.py
@@ -3,8 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world(): 
-	return "hello world!"
+def index(): 
+	return "Index Page! <p>test paragraph</p>"
+
+@app.route('/hello')
+def hello_world():
+	return "<b>hello world</b>"
 
 if __name__ == "__main__": 
 	app.run(host='0.0.0.0')
+
+
